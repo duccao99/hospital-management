762ce1d489670fecdb7266edf5f8f52666e6cdfb
Duc Cao <caovanducs@gmail.com>
2021-03-31 18:40:48 +0700

    Completed A36,37 Fix Logic bug, handle with grant option, grant privileges at column level

--- config/config.js
+++ config/config.js
@@ -9,6 +9,7 @@ module.exports = {
       all_roles: "Dba_roles",
       roleAffectDataObject: "USER_TAB_PRIVS",
       userAndTheirRole: "Dba_role_privs",
+      User_col_privs: "User_col_privs",
     },
   },
 };
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,10 +2,13 @@ const express = require("express");
 const adminModel = require("../models/admin.model");
 const router = express.Router();
 const userModel = require("../models/admin.model");
+const oracleModel = require("./../models/oracle.model.js");
 const { authUser } = require("./../middlewares/user.mdw");
+const { oracle } = require("../config/config");
 
 router.get("/", function (req, res) {
-  res.redirect("/sign-in");
+  //res.redirect("/sign-in");
+  res.redirect("/dashboard");
 });
 
 router.get("/test", function (req, res) {
@@ -116,10 +119,92 @@ router.get("/user-role", async function (req, res) {
   });
 });
 
-router.get("/grant-user-permission", authUser, function (req, res) {
+router.get("/user-column-privilege", async function (req, res) {
+  const userColPrivs = await adminModel.getUserAndTheirPrivilegesInColumn();
+
+  res.render("vwAdmin/userColPrivileges", {
+    layout: "admin",
+    userColPrivs,
+  });
+});
+
+router.get("/grant-user-permission", authUser, async function (req, res) {
+  const usernames = await adminModel.getAllUserName();
+
+  // get all column name of all table
+  const chamCongColumns = await oracleModel.getAllChamCongColumns();
+  const benhnhanColumns = await oracleModel.getAllBenhNhanColumns();
+  const hsbnColumns = await oracleModel.getAllHSBNColumns();
+  const hsdvColumns = await oracleModel.getAllHSDVColumns();
+  const hoadonColumns = await oracleModel.getAllHoaDonColumns();
+  const nhanvienColumns = await oracleModel.getAllNhanVienColumns();
+  const donviColumns = await oracleModel.getAllDonViColumns();
+  const dichvuColumns = await oracleModel.getAllDichVuColumns();
+  const ctHoaDonColumns = await oracleModel.getAllCTHOADONColumns();
+  const ctDonThuocColumns = await oracleModel.getAllCTDONTHUOCColumns();
+  const thuocColumns = await oracleModel.getAllTHUOCColumns();
+
+  const arrayColumns = [
+    {
+      tableName: "CHAMCONG",
+      columns: chamCongColumns,
+    },
+    {
+      tableName: "BENHNHAN",
+      columns: benhnhanColumns,
+    },
+    {
+      tableName: "HOSOBENHNHAN",
+      columns: hsbnColumns,
+    },
+    {
+      tableName: "HOSODICHVU",
+      columns: hsdvColumns,
+    },
+
+    {
+      tableName: "HOADON",
+      columns: hoadonColumns,
+    },
+    {
+      tableName: "NHANVIEN",
+      columns: nhanvienColumns,
+    },
+    {
+      tableName: "DONVI",
+      columns: donviColumns,
+    },
+    {
+      tableName: "DICHVU",
+      columns: dichvuColumns,
+    },
+    {
+      tableName: "CTHOADON",
+      columns: ctHoaDonColumns,
+    },
+    {
+      tableName: "CTDONTHUOC",
+      columns: ctDonThuocColumns,
+    },
+    {
+      tableName: "THUOC",
+      columns: thuocColumns,
+    },
+  ];
+
   res.render("vwAdmin/grantUserPermission", {
     layout: "admin",
     authUser: req.session.authUser,
+    arrayColumns,
+    usernames: usernames.sort((a, b) => {
+      if (a.USERNAME < b.USERNAME) {
+        return -1;
+      }
+      if (a.USERNAME > b.USERNAME) {
+        return 1;
+      }
+      return 0;
+    }),
   });
 });
 
@@ -129,16 +214,22 @@ router.post("/grant-user-permission", authUser, async function (req, res) {
       username: req.body.username,
       privilege: req.body.privilege,
       tableName: req.body.tableName,
+      withGrantOption: req.body.withGrantOption,
+      columnValue: req.body.columnValue,
     };
+    console.log(data);
 
     const status = await adminModel.grantUserPrivilege(
       data.username,
       data.privilege,
-      data.tableName
+      data.tableName,
+      data.withGrantOption,
+      data.columnValue
     );
 
     res.json({ message: "success!" });
   } catch (e) {
+    console.error(e);
     return res.status(500).json({ message: e });
   }
 });
